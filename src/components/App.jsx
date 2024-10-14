f6960c301855557a194267e55d80437ba52d210c
Arioz58 <berke.ciftci002200@gmail.com>
2024-10-14 13:33:38 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import '../styles/App.css'
+import Header from './Header'
+import Lorem from './Lorem'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+    const [isSticky, setIsSticky] = React.useState(false);
+
+    React.useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <header className={`arz-header ${isSticky ? 'sticky' : ''}`}>
+            <h1>ARZ Web Concept</h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
