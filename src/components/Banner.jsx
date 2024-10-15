bd9333c21602b16ad09c7e4e52ce62160d7cb3fb
Arioz58 <berke.ciftci002200@gmail.com>
2024-10-15 22:00:47 +0200

    added grain image and updated banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/Banner.css';
-import { useEffect } from 'react';
 
 const Banner = () => {
   return (
     <div className="arz-banner">
         <div className="circles">
+            <div className="noise"></div>
             <div className="circle one"></div>
             <div className="circle two"></div>
             <div className="circle three"></div>
