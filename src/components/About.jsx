0c1e0c59fe1b6bfb24b8b8fef93d839bcd2cfce5
Arioz58 <berke.ciftci002200@gmail.com>
2024-11-03 00:45:08 +0100

    added all assests for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import "../styles/About.css";
+import ARZmap from "../assets/ARZ-map.png";
+import ARZphone from "../assets/ARZ-phone.png";
+import ARZbrk from "../assets/ARZ-brk.png";
+import ARZlogo from "../assets/logo.png";
 
 const About = () => {
-    const firstParagraphRef = useRef(null);
-    const secondParagraphRef = useRef(null);
-    const thirdParagraphRef = useRef(null);
-
+    const gridRef = useRef(null);
     useEffect(() => {
         const handleScroll = () => {
-            const firstParagraph = firstParagraphRef.current;
-            const secondParagraph = secondParagraphRef.current;
-            const thirdParagraph = thirdParagraphRef.current;
-
+            const grid = gridRef.current;
             const windowHeight = window.innerHeight;
-            const firstParagraphPosition = firstParagraph.getBoundingClientRect().top ;
-            const secondParagraphPosition = secondParagraph.getBoundingClientRect().top ;
-            const thirdParagraphPosition = thirdParagraph.getBoundingClientRect().top ;
-
-            if (firstParagraphPosition < windowHeight / 2 ) {
-                firstParagraph.classList.add("visible");
-            }
-
-            if (secondParagraphPosition < windowHeight / 2 - 150) {
-                secondParagraph.classList.add("visible");
-                firstParagraph.classList.remove("visible");
-                firstParagraph.classList.add("hidden");
-            } else {
-                secondParagraph.classList.remove("visible");
-            }
-
-            if (thirdParagraphPosition < windowHeight / 2 - 150 ) {
-                thirdParagraph.classList.add("visible");
-                secondParagraph.classList.remove("visible");
-                secondParagraph.classList.add("hidden");
-            }
-            else {
-                thirdParagraph.classList.remove("visible");
-            }
+            const gridPosition = grid.getBoundingClientRect().top;
 
         };
 
@@ -50,25 +25,23 @@ const About = () => {
         <div className="arz-about" id="about">
             <div className="arz-about-content">
                 <h2 className="sticky">Qui sommes-nous ?</h2>
-                <p ref={firstParagraphRef} className="visible">
-                    ARZ Web Concept est une agence de développement web basée à Strasbourg spécialisée dans
-                    la création de sites web et d'applications web sur mesure. Nous
-                    accompagnons nos clients dans la réalisation de leurs projets web en
-                    leur proposant des solutions adaptées à leurs besoins.
-                </p>
-                <p ref={secondParagraphRef} className="hidden">
-                    Notre équipe est composée de développeurs web passionnés par leur
-                    métier et qui mettent tout en œuvre pour satisfaire les besoins de
-                    nos clients. Nous sommes à l'écoute de nos clients et nous les
-                    accompagnons tout au long de leur projet pour leur garantir un
-                    résultat de qualité.
-                </p>
-                <p ref={thirdParagraphRef} className="hidden">
-                    Nous mettons notre expertise à votre service pour vous aider à
-                    concrétiser vos projets web. Que vous soyez une entreprise, une
-                    association ou un particulier, nous vous accompagnons dans la
-                    réalisation de votre projet web.
-                </p>
+                <div ref={gridRef} className="arz-about-grid">
+                    <div className="arz-about-map">
+                        <img className="ARZ-logo" src={ARZlogo} alt="logo" />
+                        <p>ARZ Web Concept est une agence de <b>développement web</b> basée à <b>Strasbourg</b></p>
+                        <img className="ARZ-map" src={ARZmap} alt="map" />
+                    </div>
+                    <div className="arz-about-phone">
+                        <img className="ARZ-logo" src={ARZlogo} alt="logo" />
+                        <p>ARZ Web Concept est  spécialisée dans la création de sites web et d'applications web sur mesure</p>
+                        <img className="ARZ-phone" src={ARZphone} alt="phone" />
+                    </div>
+                    <div className="arz-about-brk">
+                        <img className="ARZ-logo" src={ARZlogo} alt="logo" />
+                        <p>Nous mettons notre expertise à votre service pour vous aider à concrétiser vos projets web. Que vous soyez une entreprise, une association ou un particulier, nous vous accompagnons dans la réalisation de votre projet web.</p>
+                        <img className="ARZ-brk" src={ARZbrk} alt="brk" />
+                    </div>
+                </div>
             </div>
         </div>
     );
