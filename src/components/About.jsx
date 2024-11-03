1d0984dd8a49efddd6b5e16c0ce59a1a0a151997
Arioz58 <berke.ciftci002200@gmail.com>
2024-11-03 22:58:58 +0100

    added scroll appear and disappear effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,45 @@
 import React, { useEffect, useRef } from "react";
-import "../styles/About.css";
 import ARZmap from "../assets/ARZ-map.png";
 import ARZphone from "../assets/ARZ-phone.png";
 import ARZbrk from "../assets/ARZ-brk.png";
 import ARZlogo from "../assets/logo.png";
+import "../styles/About.css";
 
 const About = () => {
+    const containerRef = useRef(null);
+    const mapRef = useRef(null);
+    const phoneRef = useRef(null);
+    const brkRef = useRef(null);
     const gridRef = useRef(null);
     useEffect(() => {
         const handleScroll = () => {
-            const grid = gridRef.current;
+            const container = containerRef.current;
+            const map = mapRef.current;
+            const phone = phoneRef.current;
+            const brk = brkRef.current;
+            const containerPos = container.getBoundingClientRect().top;
             const windowHeight = window.innerHeight;
-            const gridPosition = grid.getBoundingClientRect().top;
+
+            console.log(containerPos, windowHeight);
+            
+            if (containerPos < windowHeight) {
+                map.classList.remove("inactive");
+                map.classList.add("active");
+                phone.classList.remove("active");
+                phone.classList.add("inactive");
+            }
+            if (containerPos < windowHeight / 2 - 800) {
+                phone.classList.remove("inactive");
+                phone.classList.add("active");
+                brk.classList.remove("active");
+                brk.classList.add("inactive");
+            }
+            if (containerPos < windowHeight / 2 - 1800) {
+                brk.classList.remove("inactive");
+                brk.classList.add("active");
+            }
+
+            gridRef.current.style.top = `${windowHeight/5}px`;
 
         };
 
@@ -23,22 +51,22 @@ const About = () => {
 
     return (
         <div className="arz-about" id="about">
-            <div className="arz-about-content">
+            <div ref={containerRef} className="arz-about-content">
                 <h2 className="sticky">Qui sommes-nous ?</h2>
                 <div ref={gridRef} className="arz-about-grid">
-                    <div className="arz-about-map">
+                    <div ref={mapRef} className="arz-about-map inactive">
                         <img className="ARZ-logo" src={ARZlogo} alt="logo" />
-                        <p>ARZ Web Concept est une agence de <b>développement web</b> basée à <b>Strasbourg</b></p>
+                        <p>ARZ Web Concept est une agence de <b>développement web</b> basée à <b>Strasbourg</b><br />🥨</p>
                         <img className="ARZ-map" src={ARZmap} alt="map" />
                     </div>
-                    <div className="arz-about-phone">
+                    <div ref={phoneRef} className="arz-about-phone inactive">
                         <img className="ARZ-logo" src={ARZlogo} alt="logo" />
-                        <p>ARZ Web Concept est  spécialisée dans la création de sites web et d'applications web sur mesure</p>
+                        <p>ARZ Web Concept est  spécialisée dans la <b>création de sites web</b> et <b>d'applications web</b> sur mesure</p>
                         <img className="ARZ-phone" src={ARZphone} alt="phone" />
                     </div>
-                    <div className="arz-about-brk">
+                    <div ref={brkRef} className="arz-about-brk inactive">
                         <img className="ARZ-logo" src={ARZlogo} alt="logo" />
-                        <p>Nous mettons notre expertise à votre service pour vous aider à concrétiser vos projets web. Que vous soyez une entreprise, une association ou un particulier, nous vous accompagnons dans la réalisation de votre projet web.</p>
+                        <p>Nous mettons notre <b>expertise à votre service</b> pour vous aider à <b>concrétiser vos projets web.</b> Que vous soyez une entreprise, une association ou un particulier, <b>nous vous accompagnons</b> dans la <b>réalisation de votre projet web.</b></p>
                         <img className="ARZ-brk" src={ARZbrk} alt="brk" />
                     </div>
                 </div>
